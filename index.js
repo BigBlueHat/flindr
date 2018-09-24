9c5304194ee3e799e50670710ed196a0753906cf
BigBlueHat <byoung@bigbluehat.com>
2018-09-24 10:13:06 -0400

    Show error when config missing

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-const findUp = require('find-up')
-const fs = require('fs')
-const configPath = findUp.sync(['.flindr', '.flindr.json'])
+const colors = require('colors');
+const findUp = require('find-up');
+const fs = require('fs');
+const configPath = findUp.sync(['.flindr', '.flindr.json']);
+if (null === configPath) {
+  console.error('The config file (`.flindr.json`) is missing from your home directory.'.red);
+  process.exit();
+}
 const config = configPath ? JSON.parse(fs.readFileSync(configPath)) : {}
 const argv = require('yargs')
   .config(config)
